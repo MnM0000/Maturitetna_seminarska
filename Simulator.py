00da633eb5d656dca2f7ccfe2678cb9887314a70
MnM0000 <131301544+MnM0000@users.noreply.github.com>
2023-04-19 22:55:31 +0200

    Update Simulator.py

--- Simulator.py
+++ Simulator.py
@@ -18,8 +18,8 @@ white = (255, 255, 255)
 blue = 0, 0, 255
 mass_mult = 10**24
 G = 6.673 * (10**(-11))
-SCALE = 10**9
-nSCALE = SCALE
+SCALE = 10**9 # skala
+nSCALE = SCALE # pomozna skala za izracun povecave
 planetarni_podatki = {'masa': [0.330, 4.87, 5.97, 0.642, 1898, 568, 86.8, 102, 0.0130], 
                       'premer': [4879, 12104, 12756, 6792, 142984, 120536, 51118, 49528, 2376], 
                       'razdalja': [57.9, 108.2, 149.6, 228.0, 778.5, 1432.0, 2867.0, 4515.0, 5906.4],
@@ -47,14 +47,16 @@ class planet():
         self.rad = rad
 
     def update(self):
+        # posodobitev objekta
         self.x += self.x_vel
         self.y += self.y_vel
-        pg.draw.circle(screen, self.colour, (self.x, self.y), self.rad if self.rad > 2 else 2)
+        pg.draw.circle(screen, self.colour, (self.x, self.y), self.rad if self.rad > 2 else 2) # if stavek zaradi prikaza planetov (razdalje so v vesolju velike in se planetov ne bi videlo, če jih ne bi povečal)
 
     def pos(self):
-        return self.x, self.y
+        return self.x, self.y # vrne svoje koordinate
 
 def distance(item1, item2):
+    # racun razdalje in lastnosti vektorja
     x1, y1 = item1.pos()
     try:
         x2, y2 = item2.pos()
@@ -63,17 +65,18 @@ def distance(item1, item2):
         y2 = height/2
     vect = sqrt((x1-x2)**2 + (y1-y2)**2)
     length = vect*SCALE
-    dist = [length, (x1-x2)/vect, (y1-y2)/vect]
+    dist = [length, (x1-x2)/vect, (y1-y2)/vect] # razdalja v metrih, normirana vrednost v x in y smeri
     return dist
 
 def grav_effect(pl1, pl2, r):
+    # racunanje premikov in preverjanje trkov
     m1 = pl1.mass
     m2 = pl2.mass
-    v1 = G*m2*SPD/(r[0]**2)
-    v2 = G*m1*SPD/(r[0]**2)
-    s1 = [v1*r[1]*SPD/SCALE, v1*r[2]*SPD/SCALE]
+    v1 = G*m2*SPD/(r[0]**2) # izracun hitrosti 1. predmeta
+    v2 = G*m1*SPD/(r[0]**2) # izracun hitrosti 2. predmeta
+    s1 = [v1*r[1]*SPD/SCALE, v1*r[2]*SPD/SCALE] # izracun poti in mnozenje z normirano vrednostjo vektorja v x in y smeri zato, da se dobi premik v x in y smeri
     s2 = [v2*r[1]*SPD/SCALE, v2*r[2]*SPD/SCALE]
-    if r[0]/SCALE <= (pl1.rad + pl2.rad):
+    if r[0]/SCALE <= (pl1.rad + pl2.rad): # preverjanje za trk
         planets.remove(pl1)
         planets.remove(pl2)
         pl1.x_vel -= s1[0]
@@ -81,7 +84,7 @@ def grav_effect(pl1, pl2, r):
         pl2.x_vel += s2[0]
         pl2.y_vel += s2[1]
         if pl1.mass > pl2.mass:
-            vx = (m1*pl1.x_vel + m2*pl2.x_vel)/(m1+m2)
+            vx = (m1*pl1.x_vel + m2*pl2.x_vel)/(m1+m2) # izracun hitrosti novega -zdruzenega- planeta s formulo za gibalno kolicino
             vy = (m1*pl1.y_vel + m2*pl2.y_vel)/(m1+m2)
             planets.append(planet(pl1.mass + pl2.mass, pl1.x, pl1.y, vx, vy, pl1.colour, pl1.rad + pl2.rad/10))
         else:
@@ -92,6 +95,7 @@ def grav_effect(pl1, pl2, r):
         del pl2
         return 1
     else:
+        # dodajanje vrednosti poti na "hitrost" predmeta -posodabljamo zaslon nekajkrat na sekundo, zato je to dejansko pot, ki jo prištejemo vsako iteracijo, kar pomeni v bistvu hitrost
         pl1.x_vel -= s1[0]
         pl1.y_vel -= s1[1]
         pl2.x_vel += s2[0]
@@ -99,6 +103,7 @@ def grav_effect(pl1, pl2, r):
         return 0
 
 def rad_draw(pl1, czoom):
+    # izris polmera in razdalje med planetoma
     pg.draw.aaline(screen, white, (pl1.x, pl1.y), (width/2, height/2), 1)
     img = text.render(str((distance(pl1,0)[0]*czoom/SCALE).__round__(3)), 1, white)
     screen.blit(img, ((pl1.x + width/2)/2, (pl1.y + height/2)/2))
@@ -106,17 +111,21 @@ def rad_draw(pl1, czoom):
 planets = []
 Sun = planet(1.988500*(10**30), width/2, height/2, 0, 0, yellow, 1)
 if mode == 1:
-    SPD = 10*(1/FPS)*60*60*24
+    # primer 1 -> dokaz pravilnega delovanja simulacije gravitacije
     Earth = planet(5.97*mass_mult, width/2, height/2 + 147.1, 30.29*10**3*SPD/SCALE, 0, blue, 1/5)
     planets = [Sun, Earth]
 elif mode == 0:
+    # primer 0 -> primer simulacije celega osoncja (nenatancno zaradi slabih podatkov, a vseeno dovolj nazorno)
     planets = [Sun]
+
+    # dodajanje vseh planetov glede na vhodne podatke iz spletne strani Nase v seznam planetov, ki se posodabljajo in prikazujejo na zaslonu
     for i in range(len(planetarni_podatki['masa'])):
         if i % 2 == 0:
             planets.append(planet(planetarni_podatki['masa'][i]*mass_mult, width/2 - planetarni_podatki['razdalja'][i]*(10**9)/SCALE, height/2, 0, planetarni_podatki['hitrost'][i]*speed_multiplier*10**3, (random.randint(100, 255),random.randint(100, 255),random.randint(100, 255)), planetarni_podatki['premer'][i]/695700))
         else:
             planets.append(planet(planetarni_podatki['masa'][i]*mass_mult, width/2 + planetarni_podatki['razdalja'][i]*(10**9)/SCALE, height/2, 0, -planetarni_podatki['hitrost'][i]*speed_multiplier*10**3, (random.randint(100, 255),random.randint(100, 255),random.randint(100, 255)), planetarni_podatki['premer'][i]/695700))
 elif mode == 2:
+    # primer 2 -> dokaz delovanja preverjanja trkov (pri nizkih hitrostih)
     planets.append(planet(1000000*mass_mult, width/2, height/2, 0, 0, red, 10))
     planets.append(planet(1000000*mass_mult, width/2 + 100, height/2 + 100, 0, 0, white, 10))
     planets.append(planet(1000000*mass_mult, width/2 + 200, height/2 - 400, 0, 0, blue, 10))
@@ -125,10 +134,11 @@ elif mode == 2:
 
 t0 = time.time()
 while True:
-    clock.tick(FPS)
-    for event in pg.event.get():
+    clock.tick(FPS) # dodeljevanje osvezitev na sekundo
+    for event in pg.event.get(): # preverjanje za dogodek
         if event.type == pg.QUIT: sys.exit()
         elif event.type == pg.MOUSEWHEEL:
+            # povecevanje/pomanjsevanje
             if event.y < 1:
                 xzoom = zoom
                 nSCALE = SCALE + yzoom
@@ -153,24 +163,31 @@ while True:
                     SCALE = nSCALE
                     xzoom = xzoom * 1.1
 
+    # klic funkcije grav_effect - vsak element v seznamu planets z vsakim
     if len(planets) != 1:
         for i in range(len(planets)-1):
             for ii in range(i+1,len(planets)):
                 x = grav_effect(planets[i], planets[ii], distance(planets[i], planets[ii]))
-                if x == 1:
+                if x == 1: # ce pride do trka pojdi ven iz zanke
                     break   
     
     screen.fill(black)
-
+    
+    # klic funkcije rad_draw, ce je nacin enak 1
     if mode == 1:
         for x in planets:
             if x.x != width/2 or x.y != height/2:
                 rad_draw(x, nSCALE)
 
+    # posodobitev polozaja planetov in njihov izris na 'platno'
     for p in planets:
         p.update()
+    
+    # zapis casa v dnevih/10 dnevih zgoraj desno na 'platnu'
     img2 = text.render(str((time.time() - t0).__round__(2)), 1, white)
     screen.blit(img2, (10, 10))
+
+    # posodabljanje zaslona glede na 'platno'
     pg.display.update()
 
 pg.quit()
